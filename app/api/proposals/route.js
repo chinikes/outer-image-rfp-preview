b89e555dedcea3d6b3440c10553dab7651d82220
Peter Chinaka <chinikes@gmail.com>
2026-03-05 13:31:02 -0700

    Configure dynamic rendering and revalidation
    Set dynamic rendering and revalidation for proposals route.

--- app/api/proposals/route.js
+++ app/api/proposals/route.js
@@ -8,6 +8,9 @@
 import { NextResponse } from "next/server";
 import { getAllRfps, isAirtableConfigured } from "@/lib/airtable";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export async function GET() {
   try {
     if (!isAirtableConfigured()) {
